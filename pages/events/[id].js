79e3cf932784f478d0bede8ed9cf653a5ea9b855
wellzenon <wellington.zenon@gmail.com>
2021-07-21 09:32:14 -0300

    comment implementation

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,76 +1,64 @@
 import Head from "next/head";
 import NextImage from "next/image";
-import NextLink from "next/link";
-import { Box, Flex, Text, Heading, HStack } from "@chakra-ui/layout";
-import {
-  Alert,
-  AlertIcon,
-  Button,
-  IconButton,
-  Tag,
-  TagLabel,
-  Textarea,
-} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { Box, Flex, Text, Heading } from "@chakra-ui/layout";
+import { Alert, AlertIcon, Button, Textarea } from "@chakra-ui/react";
 import { HiQuestionMarkCircle } from "react-icons/hi";
 
 import Layout from "../../components/layout";
 import { useUser } from "../../hooks/useUser";
 import {
-  delPresence,
+  patchComment,
   patchPresence,
   postComment,
   postPresence,
-  putPresence,
 } from "../../lib/eventHelpers";
 import { useEffect, useState } from "react";
-import { useComments } from "../../hooks/useComments";
 import { fetcher } from "../../lib/fetcher";
+import useSWR from "swr";
+import { TextEditModal } from "../../components/TextEditModal";
 
 function Event({ event }) {
+  const { locale } = useRouter();
+  const timeOptions = { style: "long" };
+  const rtf = new Intl.RelativeTimeFormat(locale, timeOptions);
   const [info, setInfo] = useState("");
   const [comment, setComment] = useState("");
   const [presence, setPresence] = useState(undefined);
-  const { user, mutate } = useUser();
+  const { data: user, mutate: refreshUser } = useUser();
   const isAuthenticated = user?.id;
   const isOwner = isAuthenticated && user.id === event?.owner?.id;
-  // const { data, error, mutate } = useSWR(
-  //   `http://localhost:8000/comments/?event=${event}`,
-  //   fetcher
-  // );
-  // const { comments, mutate: mutateComments } = useComments({ event: event.id });
+  const comments = useSWR(
+    `http://localhost:8000/events/${event.id}/comments`,
+    fetcher
+  );
 
   useEffect(() => {
-    console.log({
-      user,
-      isAuthenticated,
-      presence,
-    });
-    isAuthenticated &&
-      setPresence(
+    setPresence(
+      isAuthenticated &&
         user.event_presences?.find(({ event: id }) => id === event.id)
-      );
-    console.log({ presence });
+    );
   }, [user]);
 
   const toogleInterest = async () => {
     const { ok, json } = presence
       ? await patchPresence(presence.id, {
-          is_interested: (!presence.is_interested && true) || false,
-          // is_confirmed: !presence.is_interested && presence.is_confirmed,
+          is_interested: !presence.is_interested,
+          is_confirmed: !presence.is_interested && presence.is_confirmed,
         })
       : await postPresence({
           user: user.id,
           event: event.id,
           is_interested: true,
         });
-
-    ok ? mutate() : setInfo(json || "Can't edit presence");
+    ok ? refreshUser() : setInfo(json || "Can't edit presence");
   };
 
   const toogleConfirmation = async () => {
-    const response = interest.presenceId
-      ? await patchPresence(interest.presenceId, {
-          is_confirmed: !interest.isConfirmed,
+    const { ok, json } = presence
+      ? await patchPresence(presence.id, {
+          is_interested: !presence.is_confirmed || presence.is_interested,
+          is_confirmed: !presence.is_confirmed,
         })
       : await postPresence({
           user: user.id,
@@ -78,20 +66,27 @@ function Event({ event }) {
           is_confirmed: true,
         });
 
-    response.ok ? mutate() : setInfo(response?.json || "Can't edit presence");
+    ok ? refreshUser() : setInfo(json || "Can't edit presence");
   };
 
   const sendComment = async () => {
-    const response = await postComment({
-      user: user.id,
+    const { ok, json } = await postComment({
       event: event.id,
       body: comment,
     });
-    response.ok
-      ? mutateComments()
-      : setInfo(response?.json || "Can't edit presence");
+    ok
+      ? comments.mutate() && setComment("")
+      : setInfo(json || "Can't send comment");
   };
 
+  const editComment = async (commentId, body) => {
+    const { ok, json } = await patchComment(commentId, {
+      body: body,
+    });
+    ok ? comments.mutate() : setInfo(json || "Can't edit comment");
+  };
+
+  console.log({ locale });
   return (
     <Layout>
       <Head>
@@ -139,16 +134,37 @@ function Event({ event }) {
       <Text>{`Price: $${event.price}`}</Text>
       <Text>{`Description: ${event.description}`}</Text>
       <Text>{`Comments:`}</Text>
-      {/* {comments &&
-        comments.map((comment, key) => (
-          <Box key={key}>
-            <Text>{`User: ${comment.user.user_name}`}</Text>
-            <Text>{`Body: ${comment.body}`}</Text>
+      {comments?.data &&
+        comments.data.map((comment, key) => (
+          <Box
+            key={key}
+            backgroundColor="blackAlpha.100"
+            margin={2}
+            padding={2}
+            rounded="md"
+          >
+            {user.id === comment.user.id && (
+              <TextEditModal
+                username={comment.user.username}
+                postId={comment.id}
+                previousComment={comment.body}
+                editComment={editComment}
+                float="right"
+              />
+            )}
+            <Text fontWeight="bold" display="inline-block">
+              {comment.user.username}
+            </Text>
+            <Text fontSize="smaller" display="inline-block" marginLeft={3}>
+              {comment.created}
+            </Text>
+            <Text>{comment.body}</Text>
           </Box>
         ))}
       {user?.id && (
         <>
           <Textarea
+            width="unset"
             margin={2}
             value={comment}
             onChange={(e) => setComment(e.target.value)}
@@ -157,7 +173,7 @@ function Event({ event }) {
             {`ADD COMMENT`}
           </Button>
         </>
-      )} */}
+      )}
     </Layout>
   );
 }
